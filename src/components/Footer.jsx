ba696696b150bd7735dff9fc3c8cb79384f843ac
Blibertore <bliberto@uci.edu>
2022-10-08 18:03:37 -0700

    Created Basic Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import PersonalLogo from "../assets/NailsByHelen_Window_Signage_r1_WHITE.png";
+import PersonalLogo2 from "../assets/NailsByHelen_Window_Signage_r1_GOLD.png";
 import "./Footer.css";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="row__footer">
+        <a href="#" className="footer__anchor">
+          <figure className="footer__logo--img-wrapper">
+            <img src={PersonalLogo} alt="" className="footer__logo--img" />
+          </figure>
+          <span class="footer__logo--popper">
+            Top
+            <ArrowUpwardIcon />
+          </span>
+        </a>
+      </div>
+    </div>
+  );
 }
